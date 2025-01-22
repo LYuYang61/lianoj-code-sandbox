c5b0848c587a60a730c89e28fe3af99355ee3cf5
lian <2242727324@qq.com>
2025-01-22 15:57:08 +0800

    代码沙箱初始化

---
+++ src/main/java/com/lian/lianojcodesandbox/LianojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.lian.lianojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LianojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LianojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lian/lianojcodesandbox/LianojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lian.lianojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LianojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
